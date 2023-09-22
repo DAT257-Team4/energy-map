a74f83b52137261b5992650066fc5570e68ee29e
Olof Forsberg <98106044+oloffor@users.noreply.github.com>
2023-09-22 11:21:34 +0200

    Add files via upload

--- src/CLI.java
+++ src/CLI.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 public class CLI {
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the energy map!");
@@ -6,21 +9,25 @@ public class CLI {
         while (true) {
             System.out.print("Please enter a country name: ");
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String country = reader.readLine();
+            String country = reader.readLine().toLowerCase();
             if (country.equals("exit")) {
                 break;
             }
             System.out.print("Please enter an energy source: ");
-            String energy = reader.readLine();
+            String energy = reader.readLine().toLowerCase();
             if (energy.equals("exit")) {
                 break;
             }
+            if (CodeFormats.COUN_MAP.containsKey(country) && CodeFormats.ENERGY_MAP.containsKey(energy)){
+                System.out.println("Displaying data for " + energy + " production in " + country + ":");
 
-            System.out.println("Displaying data for " + energy + " production in " + country + ":");
-
-            ApiRequest params = ApiRequest.ApiReqForEnergySource(energy, country);
-            InputStream xml = GetAPIData.sendAPIRequest(params);
-            ApiRequest.PrintAsString(xml);
-        }   
+                ApiRequest params = ApiRequest.ApiReqForEnergySource(energy, country);
+                InputStream xml = GetAPIData.sendAPIRequest(params);
+                ApiRequest.PrintAsString(xml);
+            } else {
+                System.out.println("Invalid input");
+            }
+            
+        }
     }
 }
