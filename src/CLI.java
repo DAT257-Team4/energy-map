54dc70946b4a41669b3deba88243ad24470f6d16
Elias H. <elias.haqiqat@protonmail.com>
2023-09-22 10:39:03 +0200

    DAT257-20 Create QueryXMLForEnergyValues

--- src/CLI.java
+++ src/CLI.java
@@ -1,4 +1,15 @@
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathExpressionException;
 import java.io.*;
+import java.util.Iterator;
+
 public class CLI {
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the energy map!");
@@ -19,14 +30,14 @@ public class CLI {
 
                 ApiRequest params = ApiRequest.ApiReqForEnergySource(energy, country);
                 InputStream xml = GetAPIData.sendAPIRequest(params);
-                ApiRequest.PrintAsString(xml);
+                XmlView.QueryXMLForEnergyValues(xml);
             } else {
                 System.out.println("Displaying data for " + energy + " production in " + country + ":");
 
                 ApiRequest params = ApiRequest.ApiReqForEnergySource(energy, country);
                 InputStream xml = GetAPIData.sendAPIRequest(params);
-                ApiRequest.PrintAsString(xml);
+                XmlView.QueryXMLForEnergyValues(xml);
             }
-        }   
+        }
     }
 }
