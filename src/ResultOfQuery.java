52744e3e108393f8c12b628366ada8afa388dfaf
Elias H. <elias.haqiqat@protonmail.com>
2023-09-28 01:50:07 +0200

    DAT257-28 cleanup

--- src/ResultOfQuery.java
+++ src/ResultOfQuery.java
@@ -1,10 +1,14 @@
 
 public class ResultOfQuery {
+    //Possibly redundant
     public String country;
+    //Possibly redundant
     public String source;
     public int value;
 
-    public ResultOfQuery(String country,String energyType){
-        value = JDBCQuery.SqlQuery(country, energyType);
+    public ResultOfQuery(String country,String source){
+        this.country = country;
+        this.source = source;
+        value = JDBCQuery.SqlQuery(country, source);
     }
 }
