6c2e148c6ead7724d8ea8ce5031ebe9378b896a2
Elias H. <elias.haqiqat@protonmail.com>
2023-09-28 01:47:35 +0200

    DAT257-28 make ResultOfQuery retrieve data from database

--- src/ResultOfQuery.java
+++ src/ResultOfQuery.java
@@ -1,8 +1,3 @@
-import java.io.IOException;
-import java.io.InputStream;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
 
 public class ResultOfQuery {
     public String country;
@@ -10,21 +5,6 @@ public class ResultOfQuery {
     public int value;
 
     public ResultOfQuery(String country,String energyType){
-        ApiRequest params = ApiRequest.ApiReqForEnergySource(energyType, country);
-        InputStream xml = GetAPIData.sendAPIRequest(params);
-
-        
-        Element energyValue;
-        try {
-            NodeList l = XmlQuery.QueryXMLForEnergyValues(xml);
-            energyValue = (Element) l.item(1);
-            String ev=energyValue.getTextContent();
-            value=Integer.parseInt(ev);
-        } catch (Exception e) {
-            value=0;
-        }
-
-        this.country=country;
-        source=energyType;
+        value = JDBCQuery.SqlQuery(country, energyType);
     }
 }
