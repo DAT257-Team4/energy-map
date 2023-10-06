098260d16c63becfea60e9c24894436a601a93ed
Elias H. <elias.haqiqat@protonmail.com>
2023-10-06 10:49:05 +0200

    DAT257-22 test that ApiRequest factory returns correct values

--- test/ApiRequestTest.java
+++ test/ApiRequestTest.java
@@ -3,11 +3,12 @@ import org.junit.Test;
 public class ApiRequestTest {
 
     /**
-     * Simply checks that the factory returns anything
+     * Simply checks that the factory returns an object with the correct values
      */
     @Test
     public void apiRequestFactoryTest() {
-        ApiRequest ap = ApiRequest.ApiReqForEnergySource("test", "test");
-        assert ap != null;
+        ApiRequest ap = ApiRequest.ApiReqForEnergySource("solar", "sweden");
+        assert (ap.getPsrType().equals(CodeFormats.ENERGY_MAP.get("solar")) &&
+                (ap.getIn_Domain().equals(CodeFormats.COUN_MAP.get("sweden"))));
     }
 }
