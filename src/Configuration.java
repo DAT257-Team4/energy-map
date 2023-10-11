c77fb7deea7bbfea0d31b1fa3f9f3efbffc4dbfb
Emil Reinfeldt <emilrei@chalmers.se>
2023-10-11 10:37:57 +0200

    DAT257-46 Added enviromental variable API key

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,18 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Configuration {
-    public static final String TOKEN=readToken();
+    public static final String TOKEN = readToken();
     public static final String BASE_URL="https://web-api.tp.entsoe.eu/api";
 
-    static String readToken() {
+    static String readTokenFromFile() {
         try {
             // read input file
             File file = new File("../token.txt");
             Scanner scanner = new Scanner(file);
             String token = scanner.nextLine();
             scanner.close();
+            System.out.println("Reading TOKEN from file.");
             return token;
         // if file couldnt be read print out error mgs
         } catch (FileNotFoundException e) {
@@ -20,6 +22,21 @@ public class Configuration {
             e.printStackTrace();
             return null;
         }
+    }
+    static String readToken() {
+        // Read the value of the environmental variable
+        Map<String, String> env = System.getenv();
+        String token = env.get("TOKEN");
+        
+        // Check if the environmental variable is defined
+        if (token.equals("insert_token") || token == null) {
+            System.out.println("Environmental variable TOKEN is not defined in enviromental variables.");
+            
+            return readTokenFromFile();
+        }
+        
+        System.out.println("TOKEN read from environment variable.");
+        return token;
     }
 }
 
